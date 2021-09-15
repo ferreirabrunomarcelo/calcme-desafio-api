525e5b5be0b810b3eed132ec65939ac5db042282
Bruno <ferreira.brunomarcelo@gmail.com>
2021-09-15 12:06:01 -0300

    feat: add spring boot resources

---
+++ src/main/java/com/calcme/desafio/vagadesenvolvedor/VagaDesenvolvedorApplication.java
@@ -0,0 +1,13 @@
+package com.calcme.desafio.vagadesenvolvedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagaDesenvolvedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagaDesenvolvedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calcme/desafio/vagadesenvolvedor/VagaDesenvolvedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcme.desafio.vagadesenvolvedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagaDesenvolvedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
