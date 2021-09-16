600ef1b372cbf766afe9e9f50c89fefac3410990
Bruno <ferreira.brunomarcelo@gmail.com>
2021-09-16 12:17:28 -0300

    feat: add model class

---
+++ src/main/java/com/calcme/desafio/vagadesenvolvedor/model/Pessoa.java
@@ -0,0 +1,80 @@
+package com.calcme.desafio.vagadesenvolvedor.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "pessoa")
+public class Pessoa {
+
+    @Id
+    private String id;
+    private String nome;
+    private String email;
+    private String telefone;
+    
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return String return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+   
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
